86909ad5c161b1e3ab9c2ec8e265887059bd910f
Artem_Hvedinich <artem33771@gmail.com>
2023-06-20 13:32:54 +0300

    add videos fix

--- src/middleware/videos/videos-validation-middleware.ts
+++ src/middleware/videos/videos-validation-middleware.ts
@@ -3,6 +3,12 @@ import { body } from "express-validator";
 
 const availableResolutionsTrueArr: string[] = ["P144", "P240", "P360", "P480", "P720", "P1080", "P1440", "P2160"];
 
+function isIsoDate(str: string) {
+  if (!/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/.test(str)) return false;
+  const d = new Date(str);
+  return !isNaN(d.getTime()) && d.toISOString() === str;
+}
+
 export const videoValidate = {
   title: body("title").trim().exists({ checkFalsy: true }).isLength({
     min: 0,
@@ -18,6 +24,6 @@ export const videoValidate = {
     .withMessage(`AvailableResolutions type: Array<${availableResolutionsTrueArr}>`),
   minAgeRestriction: body("minAgeRestriction").isFloat({ min: 1, max: 18 }).optional({ nullable: true })
     .withMessage("minAgeRestriction length should be from 1 to 18"),
-  publicationDate: body("publicationDate").isISO8601().withMessage("Publication Date format: 'yyyy-mm-dd hh:mm:ss.mil'"),
+  publicationDate: body("publicationDate").custom(v => isIsoDate(v)).withMessage("Publication Date format: 'yyyy-mm-dd hh:mm:ss.mil'"),
   canBeDownloaded: body("canBeDownloaded").isBoolean().withMessage("canBeDownloaded should be true|false")
 };
