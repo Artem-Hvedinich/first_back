45c624e4c52448890a993e7344786ade9b489daa
Artem_Hvedinich <artem33771@gmail.com>
2023-06-20 15:00:38 +0300

    add videos fix

--- src/middleware/blogs/blogs-validation-middleware.ts
+++ src/middleware/blogs/blogs-validation-middleware.ts
@@ -1,10 +1,9 @@
 import { body } from "express-validator";
 
-const checkString = (value: string) => body(value).trim().notEmpty().withMessage("Field " + value + " required to fill");
-
 export const blogValidate = {
-  name: checkString("name"),
-  description: checkString("description"),
-  websiteUrl: checkString("websiteUrl")
-
+  name: body("name").trim().notEmpty().isLength({ max: 15 }).withMessage("max length should be 15 symbols"),
+  description: body("description").trim().notEmpty().isLength({ max: 500 }).withMessage("Field description required to fill"),
+  websiteUrlLength: body("websiteUrl").trim().notEmpty().isLength({ max: 100 }).withMessage("Website url is too long"),
+  websiteUrl: body("websiteUrl").trim().custom(v =>
+    /^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/.test(v)).withMessage("website url does not match the template")
 };
--- src/router/blogs/blogs-router.ts
+++ src/router/blogs/blogs-router.ts
@@ -17,6 +17,28 @@ blogsRouter.post("/",
   blogValidate.name,
   blogValidate.description,
   blogValidate.websiteUrl,
+  blogValidate.websiteUrlLength,
+  inputValidationMiddleware,
+  async (req: Request, res: Response) => {
+    const blog = await blogsRepository.createBlog(req.body.name, req.body.description, req.body.websiteUrl);
+    res.status(201).send(blog);
+  });
+
+blogsRouter.get("/:id", async (req: Request, res: Response) => {
+  const blog = await blogsRepository.findBlog(req.params.id);
+  if (!blog) {
+    res.sendStatus(404);
+    return;
+  }
+  res.status(200).send(blog);
+});
+
+blogsRouter.put("/:id",
+  authValidate.authorization,
+  authValidationMiddleware,
+  blogValidate.name,
+  blogValidate.description,
+  blogValidate.websiteUrl,
   inputValidationMiddleware,
   async (req: Request, res: Response) => {
     const blog = await blogsRepository.createBlog(req.body.name, req.body.description, req.body.websiteUrl);
