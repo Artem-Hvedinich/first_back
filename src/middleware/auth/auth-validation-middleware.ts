d3cebd6134e0503dd7255d5259e89729d7ca2a5f
Artem_Hvedinich <artem33771@gmail.com>
2023-06-21 17:21:13 +0300

    adfsd

--- src/middleware/auth/auth-validation-middleware.ts
+++ src/middleware/auth/auth-validation-middleware.ts
@@ -5,3 +5,4 @@ export const authValidate = {
   authorization: header("authorization")
     .custom(v => v.includes("Basic ") && atob(v.slice(6)) === process.env.USER_NAME + ":" + process.env.PASSWORD)
 };
+
